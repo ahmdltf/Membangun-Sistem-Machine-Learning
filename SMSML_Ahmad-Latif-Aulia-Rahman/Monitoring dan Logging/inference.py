0fb3fb26da4a303fbf3b21f64d0010586ffc578e
Ahmad Latif <123431656+ahmdltf@users.noreply.github.com>
2026-04-19 05:20:35 +0700

    Update inference.py

--- SMSML_Ahmad-Latif-Aulia-Rahman/Monitoring dan Logging/inference.py
+++ SMSML_Ahmad-Latif-Aulia-Rahman/Monitoring dan Logging/inference.py
@@ -1,1 +1,10 @@
+import requests
 
+url = "http://127.0.0.1:1234/invocations"
+
+data = {
+    "inputs": [[0.5, 0.3, 0.2, 0.1]]
+}
+
+response = requests.post(url, json=data)
+print(response.json())
